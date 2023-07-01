dac30f66d845586e2eed3f34f6dafb0097726cec
Matheus <matheusfontenele979700@gmail.com>
2023-07-01 01:50:05 -0300

    thebar

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+const container = document.querySelector('#divAll')
 const containerConta = document.querySelector('#show-conta');
 const btnConta = document.querySelector('#btn-conta');
 const btnReturn = document.querySelector('#Npedir-conta');
@@ -12,120 +13,65 @@ function showConta() {
     containerConta.classList.toggle('hide');
 }
 
-//Função para buscar bebidas da API
-const getDrinksCard = async () => {
-    const url = '../json/bebidas.json'
+
+//Função para pegar API de cardapio
+const getCard = async () => {
+    const url = "../json/cardapio.json";
     const resp = await fetch(url);
     const data = await resp.json();
-    newDrinkCard(data)
+    creatCard(data);
 }
 
-//Função para buscar petiscos da API
-const getPetiscCard = async () => {
-    const url = '../json/petiscos.json'
-    const resp = await fetch(url);
-    const data = await resp.json();
-    newPetisCard(data)
-}
+const creatCard = (item) => {
 
-//Função para buscar hamburgues da API
-const getBurguerCard = async () => {
-    const url = '../json/burguer.json'
-    const resp = await fetch(url);
-    const data = await resp.json();
-    newBurguerCard(data)
-}
-
-
-//Função para adicionar drinks no cardapio
-const newDrinkCard = (item) => {
-
-    const size = Object.keys(item['Bebidas']).length
+    const size = Object.keys(item['menu']).length
     for (let i = 0; i < size ; i++) {
-    
-    const itensList = document.createElement('li');
-    drinksList.appendChild(itensList);
-    const spanList = document.createElement('span');
-    spanList.classList.add('itensStyle');
-    itensList.appendChild(spanList);
+        
+        const card = document.createElement('div');
+        card.classList.add('itemsCard');
+        container.appendChild(card);
 
-    const name = document.createElement('p');
-    name.innerHTML = item['Bebidas'][i].name;
-    spanList.appendChild(name);
+        const btnItens = document.createElement('button');
+        btnItens.innerText = item['menu'][i].name
+        card.appendChild(btnItens);
 
-    const desc = document.createElement('p');
-    desc.innerHTML = item['Bebidas'][i].descri;
-    spanList.appendChild(desc);
+        const ulList = document.createElement('ul');
+        ulList.classList.add('hide')
+        card.appendChild(ulList)
 
-    const valor = document.createElement('p');
-    valor.innerHTML = `${item['Bebidas'][i].valor} $R$`;
-    valor.classList.add('valorText');
-    itensList.appendChild(valor);
+        const sizeng = Object.keys(item['menu'][i].itens).length
+        for (let j = 0; j < sizeng; j++){
+            
 
-        const valorItem = item['Bebidas'][i].valor.replace(',', '.')
+            const itensList = document.createElement('li');
+            ulList.appendChild(itensList);
+
+            const spanList = document.createElement('span');
+            spanList.classList.add('itensStyle');
+            itensList.appendChild(spanList);
+
+            const name = document.createElement('p');
+            name.innerHTML = item['menu'][i].itens[j].name;
+            spanList.appendChild(name);
+
+            const desc = document.createElement('p');
+            desc.innerHTML = item['menu'][i].itens[j].descri;
+            spanList.appendChild(desc);
+
+            const valor = document.createElement('p');
+            valor.innerHTML = `${item['menu'][i].itens[j].valor} $R$`;
+            valor.classList.add('valorText');
+            itensList.appendChild(valor);
+
+            const valorItem = item['menu'][i].itens[j].valor.replace(',', '.')
         const newValorItem = Number(valorItem);
 
         console.log(newValorItem)
-        
+
+        }
     }
 }
 
-//Função para adicionar petiscos no cardapio
-const newPetisCard = (item) => {
-
-    const size = Object.keys(item['Petiscos']).length
-    for (let i = 0; i < size ; i++) {
-    
-    const itensList = document.createElement('li');
-    petisList.appendChild(itensList);
-    const spanList = document.createElement('span');
-    spanList.classList.add('itensStyle');
-    itensList.appendChild(spanList);
-
-    const name = document.createElement('p');
-    name.innerHTML = item['Petiscos'][i].name;
-    spanList.appendChild(name);
-
-    const desc = document.createElement('p');
-    desc.innerHTML = item['Petiscos'][i].descri;
-    spanList.appendChild(desc);
-
-    const valor = document.createElement('p');
-    valor.innerHTML = `${item['Petiscos'][i].valor} $R$`;
-    valor.classList.add('valorText');
-    itensList.appendChild(valor);
-    }
-}
-
-//Função para adicionar hamburguers no cardapio
-const newBurguerCard = (item) => {
-
-    const size = Object.keys(item['Hamburguer']).length
-    for (let i = 0; i < size ; i++) {
-    
-    const itensList = document.createElement('li');
-    burguerList.appendChild(itensList);
-    const spanList = document.createElement('span');
-    spanList.classList.add('itensStyle');
-    itensList.appendChild(spanList);
-
-    const name = document.createElement('p');
-    name.innerHTML = item['Hamburguer'][i].name;
-    spanList.appendChild(name);
-
-    const desc = document.createElement('p');
-    desc.innerHTML = item['Hamburguer'][i].descri;
-    spanList.appendChild(desc);
-
-    const valor = document.createElement('p');
-    valor.innerHTML = `${item['Hamburguer'][i].valor} $R$`;
-    valor.classList.add('valorText');
-    itensList.appendChild(valor);
-
-    }
-}
-
-const revisaItens = [];
 
 
 
@@ -149,6 +95,5 @@ btnReturn.addEventListener('click', () =>{
     showConta();
 })
 
-getDrinksCard()
-getPetiscCard()
-getBurguerCard()
+
+getCard();
